5f4a4bf2211d99b2719062b18803bffb43871ba5
AlexandrValov <alexandr@springdel.com>
2023-07-20 10:20:46 +0300

    updated text after buttons (life and online)

--- bot/handlers/initial_handlers.py
+++ bot/handlers/initial_handlers.py
@@ -1,15 +1,6 @@
-from create_bot import bot, dp
+from create_bot import dp
 from aiogram import types, Dispatcher
-from utils.chat_gpt_request import requests_gpt
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from bot.keyboards import inlineKb
 from db.commands import db
-
-
-# Вопрос-ответ на первый вопрос
-# is_online = ['Знакомство вживую. Подробности:',
-#              'Знакомство по сети. Подробности:']
 
 
 # Первый блок для общения вживую
@@ -17,42 +8,28 @@ from db.commands import db
 async def state_machine_start(message: types.CallbackQuery):
     await message.message.answer(
         f'Где ты находишься? Как выглядит субъект для знакомства? Что она или он делает? Девушка это или парень? '
-        f'Расскажи как можно больше подробностей?\n\n'
-        f'Например, напишите боту после выбора знакомства онлайн «Хочу начать общение с девушкой, она любит большой теннис и вечеринки, а ещё слушает хорошую музыку и живёт в Санкт Петербурге.Что ей написать?» В случае, если ответа умного бота вам не подходит, можете попросить сгенерировать новый с теми же вводными.')
-    print('Нажата кнопка')
+        f'Расскажи как можно больше подробностей – тогда ответ будет более точен.\n\n'
+        f'Например, напиши боту: "Хочу познакомиться с девушкой, она сидит в французском кафе в Санкт-Петербурге, '
+        f'пьет банановый капучино и читает книгу Рэя Брэдбери. Что ей сказать?".'
+    )
+    print('Нажата кнопка - life')
     db.add_type_of_relationship(message.from_user.id, False)
 
 
 # Второй блок для общения по сети
 @dp.callback_query_handler(text='online')
 async def state_machine_start_(message: types.CallbackQuery):
-    await message.message.answer(f'Хочешь начать общение или продолжить? Какие увлечения у субъекта твоего интереса? '
-                                 f'Девушка это или парень? Расскажи всё, что поможет подойти к ответу наиболее конкретно\n\n'
-                                 f'Например, напишите боту после выбора знакомства онлайн «Хочу начать общение с девушкой, '
-                                 f'она любит большой теннис и вечеринки, а ещё слушает хорошую музыку и живёт в Санкт Петербурге.Что ей '
-                                 f'написать?» В случае, если ответа умного бота вам не подходит, можете попросить сгенерировать новый с теми же вводными.')
-    print('Нажата кнопка')
-
+    await message.message.answer(
+        f'Хочешь начать общение или продолжить? Какие увлечения у субъекта твоего интереса? Девушка это или парень? '
+        f'Расскажи всё, что поможет подойти к ответу наиболее конкретно.\n\n'
+        f'Например, напиши боту: "Хочу начать общение с девушкой, она любит настольный теннис и вечеринки, '
+        f'а ещё слушает электронную музыку и живёт в Санкт-Петербурге. Что ей написать?" '
+        f'или продолжи разговор уже начатый: "Что ответить на «Привет, как дела»?".'
+    )
+    print('Нажата кнопка - online')
     db.add_type_of_relationship(message.from_user.id, True)
-
-
-# @dp.message_handler(content_types=types.ContentType.TEXT, state=online_date_fields.info)
-# async def load_status(message: types.Message, state: FSMContext):
-#     form = ''
-#     async with state.proxy() as data:
-#         data[0] = f'{is_online[1]} {message.text}'
-#         data_dict = dict(data)
-#         print(data_dict[0])
-#         sticker = await bot.send_sticker(chat_id=message.from_user.id,
-#                                          sticker=r"CAACAgIAAxkBAAEJk11ko1ef60EMUUHgRUS9der_oBAmlwACIwADKA9qFCdRJeeMIKQGLwQ")
-#         response = await requests_gpt(data_dict[0], message.from_user.id)
-#         await bot.delete_message(chat_id=message.from_user.id, message_id=sticker.message_id)
-#         await message.answer(response, reply_markup=inlineKb)
-#     await state.finish()
 
 
 def register_handlers_callbacks(dpt: Dispatcher):
     dpt.register_callback_query_handler(state_machine_start, text='life')
     dpt.register_callback_query_handler(state_machine_start_, text='online')
-    # dpt.register_message_handler(load_status, content_types=types.ContentType.TEXT, state=online_date_fields.info)
-    # dpt.register_message_handler(load_appearance, content_types=types.ContentType.TEXT, state=offline_date_fields.info)
