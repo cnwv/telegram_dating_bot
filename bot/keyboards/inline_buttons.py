960025783b921756e6388bc0483fc2241ad03f7f
AlexandrValov <alexandr@springdel.com>
2023-07-20 08:05:38 +0300

    updated text for end_dialog button

--- bot/keyboards/inline_buttons.py
+++ bot/keyboards/inline_buttons.py
@@ -1,6 +1,6 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-inlineKb = InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(text='Закончить чат', callback_data='end_dialog'))
+inlineKb = InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(text='Главное меню', callback_data='end_dialog'))
 #for /start command
 initial_data = InlineKeyboardMarkup(row_width=2)
 block1 = InlineKeyboardButton(text='Знакомство в жизни', callback_data='life')
@@ -8,5 +8,3 @@ block2 = InlineKeyboardButton(text='Знакомство в сети', callback_
 initial_data.row(block1, block2)
 
 #life block
-
-
